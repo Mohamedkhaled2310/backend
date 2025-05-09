3227e897a4a59731e19ef91770c471c9a29d8a20
Mohamedkhaled2310 <mohamedkhaled74222310@gmail.com>
2025-05-09 16:55:21 +0200

    just format

--- src/conatainers/Authcontainer.js
+++ src/conatainers/Authcontainer.js
@@ -1,12 +1,11 @@
-// Infrastructure
 const bcryptHasher = require('../infrastructure/bcryptHasher');
 const jwtService = require('../infrastructure/jwtService');
 const userRepo = require('../infrastructure/userRepo');
 
-// Use Cases
+
 const loginUser = require('../usecases/AuthUseCase/loginUser')(userRepo, jwtService, bcryptHasher);
 const registerUser = require('../usecases/AuthUseCase/registerUser')(userRepo, jwtService, bcryptHasher);
-// Interfaces
+
 const authController = require('../interfaces/AuthInterface/authController')(registerUser, loginUser); 
 const authRoutes = require('../interfaces/AuthInterface/authRoutes')(authController);
 
--- src/conatainers/BillContainer.js
+++ src/conatainers/BillContainer.js
@@ -1,12 +1,12 @@
 const billRepo = require('../infrastructure/billRepo');
 
-// Use Cases
+
 const createBill = require('../usecases/BillUseCase/createBillUseCase')(billRepo);
 const getPatientBills = require('../usecases/BillUseCase/getPatientBillsUseCase')(billRepo);
 const updateBill = require('../usecases/BillUseCase/updateBillUseCase')(billRepo);
 const deleteBill = require('../usecases/BillUseCase/deleteBillUseCase')(billRepo);
 
-// Controller + Routes
+
 const billController = require('../interfaces/BillInterface/billController')(
   createBill,
   getPatientBills,
